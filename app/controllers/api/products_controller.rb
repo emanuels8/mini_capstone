e8a01fe7affea136a3d70b5e5335c01a55752558
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-01-12 20:15:18 -0600

    add route for every product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,20 @@ class Api::ProductsController < ApplicationController
     @all_products = Product.all
     render "all_products.json.jb"
   end
+
+  def first_rope
+    @first_product = Product.first
+
+    render "first.json.jb"
+  end
+
+  def second_rope
+    @second_product = Product.second
+    render "second.json.jb"
+  end
+
+  def third_rope
+    @third_product = Product.third
+    render "third.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "ropes" => "products#sell_rope"
+    get "first_rope" => "products#first_rope"
+    get "second_rope" => "products#second_rope"
+    get "third_rope" => "products#third_rope"
   end
 end
