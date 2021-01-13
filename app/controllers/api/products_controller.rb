082215b04d82ae2673bba715395bd0676b016908
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-01-12 19:53:10 -0600

    create route shows all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def sell_rope
+    @all_products = Product.all
+    render "all_products.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
 #
 # Examples:
 #
-product = Product.new({ name: "Black Diamond Rope", price: 100, image_url: "https://www.rei.com/media/b1c10cfd-4bf7-477e-a7e3-ed7335e1fa2e?size=646x485", descrption:" Black Diamond 9.9mm Non-Dry Rope ")
+product = Product.new({ name: "Black Diamond Rope", price: 100, image_url: "https://www.rei.com/media/b1c10cfd-4bf7-477e-a7e3-ed7335e1fa2e?size=646x485", description: " Black Diamond 9.9mm Non-Dry Rope " })
 product.save
 
-product = Product.new({ name: "Mammut Infinity Rope", price: 300, image_url: "https://www.rei.com/media/5d3e3d4e-34c8-47d0-ac95-bc9db493f328?size=646x485", descrption:"Mammut Infinity 9.5mm x 70m Dry Rope")
-  product.save
+product = Product.new({ name: "Mammut Infinity Rope", price: 300, image_url: "https://www.rei.com/media/5d3e3d4e-34c8-47d0-ac95-bc9db493f328?size=646x485", description: "Mammut Infinity 9.5mm x 70m Dry Rope" })
+product.save
 
-  product = Product.new({ name: "Sterling Velocity", price: 250, image_url: "https://www.rei.com/media/59d689a1-7c5f-4f55-af4d-583106f568d4?size=646x485", descrption:"Sterling Velocity 9.8mm Dry-Core Rope")
-    product.save
+product = Product.new({ name: "Sterling Velocity", price: 250, image_url: "https://www.rei.com/media/59d689a1-7c5f-4f55-af4d-583106f568d4?size=646x485", description: "Sterling Velocity 9.8mm Dry-Core Rope" })
+product.save
