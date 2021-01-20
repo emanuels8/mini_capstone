e96038a06bd40919ff2e22a4713448ffcae102bf
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-01-19 21:24:18 -0600

    i added a bunch of things

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,19 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+    if params[:search]
+      @products = @products.where("name ILIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "asc"
+      @products = @products.order(:price => :asc)
+    end
+    if params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(:price => :desc)
+    end
+    if params[:sort] != "price"
+      @products = @products.order(:id => :asc)
+    end
     render "index.json.jb"
   end
 
@@ -19,8 +32,11 @@ class Api::ProductsController < ApplicationController
 
       }
     )
-    @product.save
-    render "show.json.jb"
+    if @product.save == true
+      render "show.json.jb"
+    else
+      render json: { error: "The Product did not save" }, status: 422
+    end
   end
 
   def update
@@ -29,8 +45,11 @@ class Api::ProductsController < ApplicationController
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
-    @product.save
-    render "show.json.jb"
+    if @product.save == true
+      render "show.json.jb"
+    else
+      render json: { error: "The Product did not update" }, status: 304
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: true
+  validates :description, length: { minimum: 2 }
+  validates :description, length: { maximum: 500 }
+  validates :price, numericality: { greater_than: 0 }
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
