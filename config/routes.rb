4c9934f206a3bbc457e5fdf49122dc80a4c1db79
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-01-26 18:28:22 -0600

    add authenticate action to orders

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
     post "/sessions" => "sessions#create"
 
-    get "orders" => "orders#index"
-    post "orders" => "orders#create"
+    get "/orders" => "orders#index"
+    post "/orders" => "orders#create"
     get "/orders/:id" => "orders#show"
   end
 end
